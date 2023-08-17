d55a03c92e202da7f635eba0545c49be8c3bc3c0
Mehul Verma <mehulverma04@gmail.com>
2023-08-17 10:00:54 +0530

    removing a few things from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,16 +92,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/mehulverma26"
