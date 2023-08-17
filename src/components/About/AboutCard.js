738c28744efdb71c61234af26c44229cfc8acc2e
Mehul Verma <mehulverma04@gmail.com>
2023-08-17 11:49:51 +0530

    making changes in about page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,14 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
-            <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
-            <br />
+            Hi Everyone, I am <span className="purple">Mehul Verma </span>
+            from <span className="purple"> Delhi, India.</span>
+            <br /> I am a final year student pursuing Bachelors of Computer Application (BCA)
+             at CHRIST (Deemed to be University).
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
@@ -24,7 +20,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading Books
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +30,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Mehul</footer>
         </blockquote>
       </Card.Body>
     </Card>
