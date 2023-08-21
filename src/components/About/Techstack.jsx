24cf3137c33ef5dd8b9a94632de4eddca6c3af46
Mehul Verma <mehulverma04@gmail.com>
2023-08-21 14:01:42 +0530

    adding links in  techstack

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -16,45 +16,70 @@ import {
   SiCss3,
   SiPhp,
 } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+        <Link to="https://devdocs.io/javascript/">
+          <DiJavascript1 />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <Link to="https://nodejs.org/en/docs">
+          <DiNodejs />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiNpm />
+        <Link to="https://docs.npmjs.com/">
+          <SiNpm />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <Link to="https://react.dev/reference/react">
+          <DiReact />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+        <Link to="https://git-scm.com/doc">
+          <DiGit />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
+        <Link to="https://firebase.google.com/docs">
+          <SiFirebase />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
+        <Link to="https://www.python.org/doc/">
+          <DiPython />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
+        <Link to="https://docs.oracle.com/en/java/">
+          <DiJava />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiJinja />
+        <Link to="https://jinja.palletsprojects.com/en/3.1.x/">
+          <SiJinja />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
+        <Link to="https://developer.mozilla.org/en-US/docs/Web/HTML">
+          <SiHtml5 />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiCss3 />
+        <Link to="https://developer.mozilla.org/en-US/docs/Web/CSS">
+          <SiCss3 />
+        </Link>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPhp />
+        <Link to="https://www.php.net/docs.php">
+          <SiPhp />
+        </Link>
       </Col>
     </Row>
   );
