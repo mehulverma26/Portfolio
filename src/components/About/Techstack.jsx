5654b0dbc2c5f9c406c5d22b1b309a48e0a65543
Mehul Verma <mehulverma04@gmail.com>
2023-08-21 14:22:39 +0530

    minor changes

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -21,62 +21,96 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://devdocs.io/javascript/" target="_blank">
+        <a
+          href="https://devdocs.io/javascript/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <DiJavascript1 />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://nodejs.org/en/docs" target="_blank">
+        <a href="https://nodejs.org/en/docs" target="_blank" rel="noreferrer">
           <DiNodejs />
         </a>
       </Col>
-      <Col xs={4} md={2} className="tech-icons" target="_blank">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        target="_blank"
+        rel="noreferrer"
+      >
         <a href="https://docs.npmjs.com/">
           <SiNpm />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://react.dev/reference/react" target="_blank">
+        <a
+          href="https://react.dev/reference/react"
+          target="_blank"
+          rel="noreferrer"
+        >
           <DiReact />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://git-scm.com/doc" target="_blank">
+        <a href="https://git-scm.com/doc" target="_blank" rel="noreferrer">
           <DiGit />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://firebase.google.com/docs" target="_blank">
+        <a
+          href="https://firebase.google.com/docs"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiFirebase />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://www.python.org/doc/" target="_blank">
+        <a href="https://www.python.org/doc/" target="_blank" rel="noreferrer">
           <DiPython />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://docs.oracle.com/en/java/" target="_blank">
+        <a
+          href="https://docs.oracle.com/en/java/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <DiJava />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://jinja.palletsprojects.com/en/3.1.x/" target="_blank">
+        <a
+          href="https://jinja.palletsprojects.com/en/3.1.x/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiJinja />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiHtml5 />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiCss3 />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://www.php.net/docs.php" target="_blank">
+        <a href="https://www.php.net/docs.php" target="_blank" rel="noreferrer">
           <SiPhp />
         </a>
       </Col>
--- src/components/About/Toolstack.jsx
+++ src/components/About/Toolstack.jsx
@@ -11,22 +11,34 @@ function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://www.microsoft.com/en-us/windows?r=1" target="_blank">
+        <a
+          href="https://www.microsoft.com/en-us/windows?r=1"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiWindows />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://code.visualstudio.com/" target="_blank">
+        <a
+          href="https://code.visualstudio.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiVisualstudiocode />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://www.kali.org/" target="_blank">
+        <a href="https://www.kali.org/" target="_blank" rel="noreferrer">
           <SiKalilinux />
         </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href="https://www.eclipse.org/downloads/" target="_blank">
+        <a
+          href="https://www.eclipse.org/downloads/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SiEclipseide />
         </a>
       </Col>
