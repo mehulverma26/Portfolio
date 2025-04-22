d4fd8bdc13090dc1543ba7c77675e09711b2f298
Mehul Verma <mehulverma04@gmail.com>
2025-04-22 04:15:35 +0100

    adding updated and recent CV

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./Resume.pdf";
+import pdf from "./Updated_Resume_MV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
