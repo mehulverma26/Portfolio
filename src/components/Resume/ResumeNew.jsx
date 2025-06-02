74cd4d3fac0d40b7e8183a42c28ed1c168e0788e
Mehul Verma <mehulverma04@gmail.com>
2025-06-02 20:13:34 +0100

    fixing up the resume

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./Updated_Resume_MV 02-06-25.pdf";
+import pdf from "./Mehul_Verma_Resume_2025.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
